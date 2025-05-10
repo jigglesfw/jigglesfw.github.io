3b437feadb444627c9c2d36c27b3e34d5d9781b1
Jaime Lopez <54753626+jaimelopez11@users.noreply.github.com>
2025-05-10 14:27:46 -0500

    Revert "Test"
    This reverts commit 045cbb101f72112ca28bbb5bdedcf4610df5d645.

--- list-obj-files.php
+++
@@ -1,7 +0,0 @@
-<?php
-$directory = "OBJs/";
-$files = array_values(array_filter(scandir($directory), function($file) use ($directory) {
-    return pathinfo($file, PATHINFO_EXTENSION) === 'obj';
-}));
-echo json_encode($files);
-?>
